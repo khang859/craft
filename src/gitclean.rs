526b66836377a760c557bbf4d9497d723026ee2a
khang <enone859@gmail.com>
2024-02-09 08:53:29 -0500

    parse_line tests and complete map chain

--- src/gitclean.rs
+++ src/gitclean.rs
@@ -1,11 +1,15 @@
 use std::{fs::File, io::Read};
 
-use anyhow::Error;
+use anyhow::{Error, anyhow};
 
+#[derive(Debug, PartialEq)]
 enum GitType {
     Clean,
     Checkout,
+    Unstable,
 }
+
+#[derive(Debug)]
 pub struct GitPath {
     git_type: GitType,
     path: String,
@@ -19,24 +23,44 @@ pub fn handle_gitclean(path: String) -> Result<(), Error> {
     let _: Vec<GitPath> = content
         .lines()
         .map(|line| {
-            let _ = parse_line(line);
-            return GitPath {
-                git_type: GitType::Checkout,
-                path: String::from("/path/file.txt"),
-            };
-        })
+            let parsed_line = parse_line(line);
+            match parsed_line {
+                Ok(parsed) => {
+                    return parsed;
+                },
+                Err(e) => {
+                    return GitPath {
+                        git_type: GitType::Unstable,
+                        path: format!("{}", e),
+                    }
+                }
+            }
+        }) 
         .collect();
 
     return Ok(());
 }
 
-fn parse_line(line: &str) -> Result<String, Error> {
-    println!("{:?}", line);
+fn parse_line(line: &str) -> Result<GitPath, Error> {
     let line_parse_result: Vec<&str> = line.split(":").collect();
 
-    println!("{:?}", line_parse_result);
-
-    return Ok(String::from("test"));
+    if line_parse_result.len() > 1 {
+        if let Some(line_path) = line_parse_result.get(1) {
+            return Ok(GitPath {
+                git_type: GitType::Checkout,
+                path: line_path.trim().to_string(),
+            });
+        }
+    } else {
+        if let Some(line_path) = line_parse_result.get(0) {
+            return Ok(GitPath {
+                git_type: GitType::Clean,
+                path: line_path.trim().to_string(),
+            });
+        }
+    }
+    
+    return Err(anyhow!("unabled to parse line"));
 }
 
 #[cfg(test)]
@@ -55,5 +79,26 @@ mod gitclean_tests {
         let file_content = "modified: /test/file.txt\ndeleted: /another/test/file/this/time/is.yml\n/some/other/file/now.yml\n";
         let _ = writeln!(file, "{}", file_content);
         return Ok(());
+    }
+
+    #[test]
+    fn test_parse_line() {
+        let file_path = "/test/file.txt";
+        let modified_str = format!("modified: {}", &file_path);
+        let parsed_item = parse_line(&modified_str).expect("unabled to parse modified line");
+
+        assert_eq!(parsed_item.git_type, GitType::Checkout);
+        assert_eq!(parsed_item.path, file_path);
+
+        let deleted_str = format!("deleted: {}", &file_path); 
+        let parsed_item = parse_line(&deleted_str).expect("unabled to parse deleted line");
+        
+        assert_eq!(parsed_item.git_type, GitType::Checkout);
+        assert_eq!(parsed_item.path, file_path);
+
+        let parsed_item = parse_line(&file_path).expect("unabled to parse none line");
+
+        assert_eq!(parsed_item.git_type, GitType::Clean);
+        assert_eq!(parsed_item.path, file_path);
     }
 }
